244ff2a2b722f4cd0650ba8fb6ea998295fff226
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2012-07-14 02:36:37 -0300

    changing structure

---
+++ fclgen/settings/env_dev.py
@@ -0,0 +1,17 @@
+from utils import *
+
+SITE_ID = 1
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+SERVE_MEDIA = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': LOCAL('db.sqlite'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
---
+++ fclgen/settings/installed_apps.py
@@ -0,0 +1,12 @@
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'generator',
+)
+
